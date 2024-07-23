493e9ecd83ed1051e8127a0192bb354e81f95a07
Kim Alfred A. Molina <kimalfred22molina@gmail.com>
2024-07-23 20:17:31 +0800

    Added Home and Adjust Navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
+import Navbar from './components/Navbar';
+import Home from './components/Home';
 
 const App = () => {
   return (
     <div>
-      <div className="absolute top-0 z-[-2] h-screen w-screen bg-neutral-950 bg-[radial-gradient(ellipse_80%_80%_at_50%_-20%,rgba(120,119,198,0.3),rgba(255,255,255,0))]">
-
+      <div className="absolute inset-0 -z-10 h-full w-full items-center px-5 py-24 [background:radial-gradient(125%_125%_at_50%_10%,#000_40%,#63e_100%)]">
       </div>
 
       <div>
         <Navbar />
+        <Home />
       </div>
     </div>
   )
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,28 @@
-import React from 'react'
+import logo from '../assets/k.png';
 
 const Navbar = () => {
   return (
-    <div className="">
-      
+    <div className="fixed top-0 left-0 w-full text-white z-50 font-poppins bg-neutral-700">
+      <div className="max-w-7xl mx-auto px-6 sm:px-8 lg:px-10">
+        <div className="flex justify-between items-center h-20">
+          <div className="flex-shrink-0">
+            <a href="#">
+              <img src={logo} alt="Logo" className="h-12" />
+            </a>
+          </div>
+          <div className="flex-1 flex justify-center items-center space-x-12 text-lg">
+            <a href="#home" className="hover:text-black font-bold">Home</a>
+            <a href="#about" className="hover:text-black font-bold">About</a>
+            <a href="#info" className="hover:text-black font-bold">Skills</a>
+            <a href="#services" className="hover:text-black font-bold">Experience</a>
+            <a href="#pricing" className="hover:text-black font-bold">Projects</a>
+            <a href="#contact" className="hover:text-black font-bold">Contact</a>
+          </div>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Navbar
+export default Navbar;
+
