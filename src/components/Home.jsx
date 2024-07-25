2fb2e876c7f87a89ae3910cbd24c77824b627e45
Kim Alfred A. Molina <kimalfred22molina@gmail.com>
2024-07-25 20:13:48 +0800

    Added Motion

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -42,7 +42,7 @@ const Home = () => {
       <div className="flex space-x-4">
         <button className="group relative">
           <div className="relative z-10 inline-flex h-12 items-center justify-center overflow-hidden rounded-md border border-neutral-200 bg-transparent px-6 font-medium text-neutral-600 transition-all duration-300 group-hover:-translate-x-3 group-hover:-translate-y-3">
-            <a href="/path/to/your/cv.pdf" download className="text-white hover:text-red-600 transition-colors duration-300">Download CV</a>  <FaDownload color='white'/>
+            <a href="/path/to/your/cv.pdf" download className="text-white hover:text-lime-400 transition-colors duration-300">Download CV</a>  <FaDownload color='white'/>
           </div>
           <div className="absolute inset-0 z-0 h-full w-full rounded-md transition-all duration-300 group-hover:-translate-x-3 group-hover:-translate-y-3 group-hover:[box-shadow:5px_5px_#a3a3a3,10px_10px_#d4d4d4,15px_15px_#e5e5e5]"></div>
         </button>
