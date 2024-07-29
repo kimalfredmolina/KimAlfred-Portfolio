7b2d97cf9858183f51c159109040654a6f7981fc
Kim Alfred A. Molina <kimalfred22molina@gmail.com>
2024-07-29 19:37:45 +0800

    Added Projects

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -8,6 +8,7 @@ import pic5 from "../assets/stud.png"
 import pic6 from "../assets/genshin.png"
 import pic7 from "../assets/atm.png"
 import pic8 from "../assets/book.png"
+import pic9 from "../assets/horizon.png"
 /*icon png's*/
 import icon1 from "../assets/icons/html.png"
 import icon2 from "../assets/icons/css.png"
@@ -18,6 +19,7 @@ import icon6 from "../assets/icons/py.webp"
 import icon7 from "../assets/icons/react.png"
 import icon8 from "../assets/icons/tail.svg"
 import icon9 from "../assets/icons/django.svg"
+import icon10 from "../assets/icons/figma.png"
 
 const ProjectCard = ({ image, title, description, icons, link }) => {
   return (
@@ -36,7 +38,7 @@ const ProjectCard = ({ image, title, description, icons, link }) => {
             ))}
           </div>
           <a href={link} class="mt-8 group relative inline-flex h-12 items-center justify-center overflow-hidden rounded-md bg-neutral-950 px-6 font-medium text-neutral-200 duration-500"><div class="translate-x-0 opacity-100 transition group-hover:-translate-x-[150%] group-hover:opacity-0">
-              View Project on Github
+              View Project
           </div><div class="absolute translate-x-[150%] opacity-0 transition group-hover:translate-x-0 group-hover:opacity-100"><svg width="15" height="15" viewBox="0 0 15 15" fill="none" xmlns="http://www.w3.org/2000/svg" class="h-6 w-6"><path d="M8.14645 3.14645C8.34171 2.95118 8.65829 2.95118 8.85355 3.14645L12.8536 7.14645C13.0488 7.34171 13.0488 7.65829 12.8536 7.85355L8.85355 11.8536C8.65829 12.0488 8.34171 12.0488 8.14645 11.8536C7.95118 11.6583 7.95118 11.3417 8.14645 11.1464L11.2929 8H2.5C2.22386 8 2 7.77614 2 7.5C2 7.22386 2.22386 7 2.5 7H11.2929L8.14645 3.85355C7.95118 3.65829 7.95118 3.34171 8.14645 3.14645Z" fill="currentColor" fill-rule="evenodd" clip-rule="evenodd"></path></svg></div>
           </a>
         </div>
@@ -82,6 +84,13 @@ const Projects = () => {
         link: 'https://github.com/kimalfred/CMD-Tutorial-Website',
     },
     {
+        image: pic9,
+        title: 'HorizontalX Operating System Design',
+        description: 'HorizonX is a groundbreaking advancement in user interfaces and system functionalities, seamlessly blending the best features of both Windows and Mac OS.',
+        icons: [ icon10 ],
+        link: 'https://www.figma.com/community/file/1377651731493359499/horizontalx-operating-system',
+    },
+    {
         image: pic5,
         title: 'Student Profile Managemnet System',
         description: 'Simple Student Profile Management System Using Django & HTML',
@@ -98,7 +107,7 @@ const Projects = () => {
     {
         image: pic7,
         title: 'ATM Interface System',
-        description: 'user-friendly and intuitive interface using Java Swing. Making the UI resemble a real ATM familiarity was the difficult part of this project.',
+        description: 'User-friendly and intuitive interface using Java Swing. Making the UI resemble a real ATM familiarity was the difficult part of this project.',
         icons: [ icon4, icon5 ],
         link: 'https://github.com/kimalfred/ATM-Interface-System',
     },
