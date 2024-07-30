cde649da0c1df20f2e9f7cec97304c2719d91f22
Kim Alfred A. Molina <kimalfred22molina@gmail.com>
2024-07-30 19:45:38 +0800

    Added Motion

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,3 +1,4 @@
+import { motion } from "framer-motion";
 import logo1 from "../assets/logo/fb.png"
 import logo2 from "../assets/logo/insta.webp"
 import logo3 from "../assets/logo/github.webp"
@@ -8,9 +9,19 @@ const Contact = () => {
   return (
     <div id="contact" className="min-h-screen flex items-center justify-center">
       <div className="container mx-auto px-4">
-        <h2 className="text-white text-5xl font-bold text-center mb-16">Contact Me</h2>
+        <motion.h2 
+        whileInView={{opacity: 1, y: 0}}
+        initial={{opacity: 0 , y: -100}}
+        transition={{duration: 1.5}}
+        className="text-white text-5xl font-bold text-center mb-16">Contact Me
+        </motion.h2>
+        
         <div className="flex flex-col md:flex-row justify-between items-center">
-          <div className="text-white mb-8 md:mb-0 md:w-1/2">
+          <motion.div 
+          whileInView={{opacity: 1, x: 0}}
+          initial={{opacity: 0 , x: -100}}
+          transition={{duration: 1}}
+          className="text-white mb-8 md:mb-0 md:w-1/2">
             <h3 className="text-2xl mb-4">Let’s Discuss on something cool together on...</h3>
             <div className="flex space-x-4 mb-8">
               <a href="https://www.facebook.com/kimalfred.amolina"><img src={logo1} alt="Facebook" className="w-8 h-8"/></a>
@@ -27,8 +38,12 @@ const Contact = () => {
               <button className="bg-gray-700 text-white py-2 px-4 rounded hover:text-red-600 transition-colors duration-300">Ecommerce Website</button>
               <button className="bg-gray-700 text-white py-2 px-4 rounded hover:text-red-600 transition-colors duration-300">Backend</button>
             </div>
-          </div>
-          <div className="bg-gray-700 p-8 rounded-lg md:w-1/2 w-full">
+          </motion.div>
+          <motion.div
+          whileInView={{opacity: 1, x: 0}}
+          initial={{opacity: 0 , x: 100}}
+          transition={{duration: 1}}
+          className="bg-gray-700 p-8 rounded-lg md:w-1/2 w-full">
             <form>
               <div className="mb-4">
                 <label className="block text-white text-sm font-bold mb-2" htmlFor="name">Name</label>
@@ -52,7 +67,7 @@ const Contact = () => {
                 </div><div class="absolute translate-y-[150%] opacity-0 transition group-hover:translate-y-0 group-hover:opacity-100"><svg width="15" height="15" viewBox="0 0 15 15" fill="none" xmlns="http://www.w3.org/2000/svg" class="h-6 w-6"><path d="M7.5 2C7.77614 2 8 2.22386 8 2.5L8 11.2929L11.1464 8.14645C11.3417 7.95118 11.6583 7.95118 11.8536 8.14645C12.0488 8.34171 12.0488 8.65829 11.8536 8.85355L7.85355 12.8536C7.75979 12.9473 7.63261 13 7.5 13C7.36739 13 7.24021 12.9473 7.14645 12.8536L3.14645 8.85355C2.95118 8.65829 2.95118 8.34171 3.14645 8.14645C3.34171 7.95118 3.65829 7.95118 3.85355 8.14645L7 11.2929L7 2.5C7 2.22386 7.22386 2 7.5 2Z" fill="currentColor" fill-rule="evenodd" clip-rule="evenodd"></path></svg></div></button>
               </div>
             </form>
-          </div>
+          </motion.div>
         </div>
       </div>
     </div>
