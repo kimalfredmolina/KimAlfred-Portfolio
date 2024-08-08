adc7af706cb35bed633fa53d3bbbbd7c27c71d80
Kim Alfred <kimalfred22molina@gmail.com>
2024-08-08 19:38:21 +0800

    Added Responsive

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -38,9 +38,15 @@ const ProjectCard = ({ image, title, description, icons, link }) => {
               <img key={index} src={icon} alt={`icon-${index}`} className="w-6 h-6" />
             ))}
           </div>
-          <a href={link} class="mt-8 group relative inline-flex h-12 items-center justify-center overflow-hidden rounded-md bg-neutral-950 px-6 font-medium text-neutral-200 duration-500"><div class="translate-x-0 opacity-100 transition group-hover:-translate-x-[150%] group-hover:opacity-0">
-              View Project
-          </div><div class="absolute translate-x-[150%] opacity-0 transition group-hover:translate-x-0 group-hover:opacity-100"><svg width="15" height="15" viewBox="0 0 15 15" fill="none" xmlns="http://www.w3.org/2000/svg" class="h-6 w-6"><path d="M8.14645 3.14645C8.34171 2.95118 8.65829 2.95118 8.85355 3.14645L12.8536 7.14645C13.0488 7.34171 13.0488 7.65829 12.8536 7.85355L8.85355 11.8536C8.65829 12.0488 8.34171 12.0488 8.14645 11.8536C7.95118 11.6583 7.95118 11.3417 8.14645 11.1464L11.2929 8H2.5C2.22386 8 2 7.77614 2 7.5C2 7.22386 2.22386 7 2.5 7H11.2929L8.14645 3.85355C7.95118 3.65829 7.95118 3.34171 8.14645 3.14645Z" fill="currentColor" fill-rule="evenodd" clip-rule="evenodd"></path></svg></div>
+          <a href={link} className="mt-8 group relative inline-flex h-12 items-center justify-center overflow-hidden rounded-md bg-neutral-950 px-6 font-medium text-neutral-200 duration-500">
+              <div className="translate-x-0 opacity-100 transition group-hover:-translate-x-[150%] group-hover:opacity-0">
+                  View Project
+              </div>
+              <div className="absolute translate-x-[150%] opacity-0 transition group-hover:translate-x-0 group-hover:opacity-100">
+                  <svg width="15" height="15" viewBox="0 0 15 15" fill="none" xmlns="http://www.w3.org/2000/svg" className="h-6 w-6">
+                      <path d="M8.14645 3.14645C8.34171 2.95118 8.65829 2.95118 8.85355 3.14645L12.8536 7.14645C13.0488 7.34171 13.0488 7.65829 12.8536 7.85355L8.85355 11.8536C8.65829 12.0488 8.34171 12.0488 8.14645 11.8536C7.95118 11.6583 7.95118 11.3417 8.14645 11.1464L11.2929 8H2.5C2.22386 8 2 7.77614 2 7.5C2 7.22386 2.22386 7 2.5 7H11.2929L8.14645 3.85355C7.95118 3.65829 7.95118 3.34171 8.14645 3.14645Z" fill="currentColor" fill-rule="evenodd" clip-rule="evenodd"></path>
+                  </svg>
+              </div>
           </a>
         </div>
     </motion.div>
@@ -100,7 +106,7 @@ const Projects = () => {
     },
     {
         image: pic5,
-        title: 'Student Profile Managemnet System',
+        title: 'Student Profile Management System',
         description: 'Simple Student Profile Management System Using Django & HTML',
         icons: [ icon9, icon1 ],
         link: 'https://github.com/kimalfred/Student-Profile-Management',
@@ -130,14 +136,14 @@ const Projects = () => {
 
   return (
     <div id="projects" className="mt-16">
-      <div className="container mx-auto">
+      <div className="container mx-auto px-4">
         <motion.h2 
         whileInView={{opacity: 1, y: 0}}
         initial={{opacity: 0 , y: -100}}
         transition={{duration: 1.5}}
-        className="my-20 text-center text-6xl font-bold text-white">Projects</motion.h2>
+        className="my-20 text-center text-4xl md:text-6xl font-bold text-white">Projects</motion.h2>
         
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
+        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8">
           {projects.map((project, index) => (
             <ProjectCard
               key={index}
