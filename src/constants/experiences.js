a923a3e9f529c7e1bc5b4dd8c8c130554244a5f8
Kim Alfred A. Molina <kimalfred22molina@gmail.com>
2026-05-23 21:40:44 +0800

    Change description

--- src/constants/experiences.js
+++ src/constants/experiences.js
@@ -4,9 +4,9 @@ export const EXPERIENCES = [
     role: "Software Developer Intern",
     company: "Inspire Holdings Inc.",
     description: `
-      • Worked as a Full Stack Web Developer for a client-based Pawnshop Management System for JCLB Pawnshop, mainly focusing on backend development using NestJS, Prisma ORM, Supabase, and TypeScript while collaborating in an Agile/SDLC environment.
+      • Worked as a Full Stack Web Developer for a client-based Pawnshop Management System, primarily focusing on backend development using NestJS, Prisma ORM, Supabase, and TypeScript while collaborating in an Agile/SDLC environment.
       • Implemented Prisma ORM integration across the backend system to improve database management, scalability, maintainability, and overall application security.
       • Enhanced web application security by implementing Row-Level Security (RLS) in Supabase to strengthen data protection and reduce unauthorized access risks from attackers or malicious users.`,
-    technologies: ["Next.js", "Nest.js", "TypeScript", "Supabase", "Prisma ORM", "Tailwind CSS", "Git", "Github", "Figma", "SDLC", "Agile",],
+    technologies: ["Next.js", "Nest.js", "TypeScript", "Supabase", "Prisma ORM", "Tailwind CSS", "Postman", "Git", "Github", "Figma", "SDLC", "Agile",],
   },
 ];
