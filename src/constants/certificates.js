881aa7b81c562d027f9d7e987eca0499854dd74b
Kim Alfred A. Molina <kimalfred22molina@gmail.com>
2026-06-28 16:05:22 +0800

    Added new certificate

--- src/constants/certificates.js
+++ src/constants/certificates.js
@@ -1,19 +1,29 @@
 import AppCon from '../assets/certificates/AppCon.jpg'
 import IMCEST from '../assets/certificates/IMCEST.jpg'
 import MongoDB from '../assets/certificates/MongoDB.jpg'
+import Coursera1 from '../assets/certificates/Molina-Coursera1.jpg'
 
 export const certificates = [
   {
     id: 1,
-    title: 'AI & Development Webinar',
-    issuer: 'OTIS Philippines Inc.',
-    date: 'November 2024',
-    image:  AppCon,
-    credentialUrl: 'https://www.facebook.com/OTisPhilippinesInc',
-    skills: ['AI', 'Development']
+    title: 'Foundations of Project Management',
+    issuer: 'Google',
+    date: ' 2026',
+    image: Coursera1,
+    credentialUrl: 'https://coursera.org/share/cc80d642e85cb7d6fba3b43a24cfa211',
+    skills: ['Project Management', 'Google']
   },
   {
     id: 2,
+    title: 'Monitoring MongoDB with Built-in Tools',
+    issuer: 'MongoDB',
+    date: 'May 2026',
+    image: MongoDB,
+    credentialUrl: 'https://www.credly.com/badges/63a7360f-fa59-4211-9d0a-631ab60ee97f/public_url',
+    skills: ['Monitoring', 'MongoDB']
+  },
+  {
+    id: 3,
     title: 'IMCEST Thesis Paper Presentation',
     issuer: 'IMCEST',
     date: 'November 2025',
@@ -22,12 +32,12 @@ export const certificates = [
     skills: ['Thesis Paper']
   },
   {
-    id: 3,
-    title: 'Monitoring MongoDB with Built-in Tools',
-    issuer: 'MongoDB',
-    date: 'May 2026',
-    image: MongoDB,
-    credentialUrl: 'https://www.credly.com/badges/63a7360f-fa59-4211-9d0a-631ab60ee97f/public_url',
-    skills: ['Monitoring', 'MongoDB']
+    id: 4,
+    title: 'AI & Development Webinar',
+    issuer: 'OTIS Philippines Inc.',
+    date: 'November 2024',
+    image: AppCon,
+    credentialUrl: 'https://www.facebook.com/OTisPhilippinesInc',
+    skills: ['AI', 'Development']
   },
 ];
