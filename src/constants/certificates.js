4745a3019703eb7356a153b65c23752e4579afbf
Kim Alfred A. Molina <kimalfred22molina@gmail.com>
2026-06-30 19:42:06 +0800

    Added new Certificate

--- src/constants/certificates.js
+++ src/constants/certificates.js
@@ -2,19 +2,26 @@ import AppCon from '../assets/certificates/AppCon.jpg'
 import IMCEST from '../assets/certificates/IMCEST.jpg'
 import MongoDB from '../assets/certificates/MongoDB.jpg'
 import Coursera1 from '../assets/certificates/Molina-Coursera1.jpg'
+import Coursera2 from '../assets/certificates/Molina-Coursera2.jpg'
 
 export const certificates = [
   {
-    id: 1,
+    title: 'Project Initiation: Starting a Successful Project',
+    issuer: 'Google',
+    date: 'June 2026',
+    image: Coursera2,
+    credentialUrl: 'https://www.coursera.org/account/accomplishments/verify/ABB11N0C8ZIF',
+    skills: ['Project Initiation', 'Google']
+  },
+  {
     title: 'Foundations of Project Management',
     issuer: 'Google',
-    date: ' 2026',
+    date: 'June 2026',
     image: Coursera1,
     credentialUrl: 'https://coursera.org/share/cc80d642e85cb7d6fba3b43a24cfa211',
     skills: ['Project Management', 'Google']
   },
   {
-    id: 2,
     title: 'Monitoring MongoDB with Built-in Tools',
     issuer: 'MongoDB',
     date: 'May 2026',
@@ -23,7 +30,6 @@ export const certificates = [
     skills: ['Monitoring', 'MongoDB']
   },
   {
-    id: 3,
     title: 'IMCEST Thesis Paper Presentation',
     issuer: 'IMCEST',
     date: 'November 2025',
@@ -32,7 +38,6 @@ export const certificates = [
     skills: ['Thesis Paper']
   },
   {
-    id: 4,
     title: 'AI & Development Webinar',
     issuer: 'OTIS Philippines Inc.',
     date: 'November 2024',
