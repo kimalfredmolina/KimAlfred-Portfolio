6d4a59c68e13a33c223b25c87a2b41c4b178e6bc
Kim Alfred A. Molina <kimalfred22molina@gmail.com>
2026-07-14 21:11:38 +0800

    Change Resume and experience

--- src/constants/experiences.js
+++ src/constants/experiences.js
@@ -1,12 +1,22 @@
 export const EXPERIENCES = [
   {
-    year: "Feb 2026 - Jun 2026",
+    year: "Jun 2026 - Present",
+    role: "Software Developer",
+    company: "Inspire Holdings Inc.",
+    description: `
+      • Develop and maintain backend features, RESTful APIs, and database solutions for multiple web applications and internal systems using NestJS, TypeScript, Prisma ORM, Supabase, and PostgreSQL.
+      • Implement new features, resolve production issues, and optimize backend performance to ensure reliable and scalable applications in an Agile/SDLC environment.
+      • Implemented endpoint authorization and access control by securing RESTful APIs with authentication and role-based authorization, returning appropriate HTTP 401 Unauthorized responses for unauthenticated requests.`,
+    technologies: ["Next.js", "Nest.js", "TypeScript", "Supabase", "Prisma ORM", "Tailwind CSS", "Postman", "Git", "Github", "Figma", "SDLC", "Agile",],
+  },
+  {
+    year: "Feb 2026 - May 2026",
     role: "Software Developer Intern",
     company: "Inspire Holdings Inc.",
     description: `
-      • Worked as a Full Stack Web Developer for a client-based Pawnshop Management System, primarily focusing on backend development using NestJS, Prisma ORM, Supabase, and TypeScript while collaborating in an Agile/SDLC environment.
-      • Implemented Prisma ORM integration across the backend system to improve database management, scalability, maintainability, and overall application security.
-      • Enhanced web application security by implementing Row-Level Security (RLS) in Supabase to strengthen data protection and reduce unauthorized access risks from attackers or malicious users.`,
+      • Developed backend features and RESTful APIs for a client-based Pawnshop Management System using NestJS, Prisma ORM, Supabase, and TypeScript while collaborating in an Agile/SDLC environment.
+      • Integrated Prisma ORM with PostgreSQL and Supabase to improve database management, maintainability, and application scalability.
+      • Implemented Row-Level Security (RLS) policies in Supabase to enforce role-based data access and strengthen application security.`,
     technologies: ["Next.js", "Nest.js", "TypeScript", "Supabase", "Prisma ORM", "Tailwind CSS", "Postman", "Git", "Github", "Figma", "SDLC", "Agile",],
   },
 ];
